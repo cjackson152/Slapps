c4e3469281e27062a8db11a17235fb2d80af9018
Cole Jackson <cole_jackson@live.com>
2020-04-11 11:11:39 -0700

    calendar tiles now have an onclick function

--- calendar.js
+++ calendar.js
@@ -1,34 +1,45 @@
+//variables for dates
 today = new Date();
 currentMonth = today.getMonth();
 currentYear = today.getFullYear();
 selectYear = document.getElementById("year");
 selectMonth = document.getElementById("month");
 
+//Month Names for display
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 
+//
 monthAndYear = document.getElementById("monthAndYear");
 showCalendar(currentMonth, currentYear);
 
+//next month button
 function next() {
     currentYear = (currentMonth === 11) ? currentYear + 1 : currentYear;
     currentMonth = (currentMonth +1) % 12;
     showCalendar(currentMonth, currentYear);
 }
 
+//last month button
 function previous() {
     currentYear = (currentMonth === 0) ? currentYear - 1 : currentYear;
     currentMonth = (currentMonth === 0) ? 11 : currentMonth - 1;
     showCalendar(currentMonth, currentYear);
 }
 
+//jump to month/ year
 function choose() {
     currentYear = parseInt(selectYear.value);
     currentMonth = parseInt(selectMonth.value);
     showCalendar(currentMonth, currentYear);
 }
 
+// function to click tds through calendar
+$(document).on('click', 'td', function(){
+    console.log($(this))
+})
 
 
+//generates calendar
 function showCalendar(month, year) {
 
 let firstDay = (new Date(year, month)).getDay();
@@ -42,13 +53,13 @@ selectYear.value = year;
 selectMonth.value = month;
 
 
-
+//creates calendar rows
 let date = 1;
 for (let i = 0; i < 6; i++) {
     let row = document.createElement("tr");  
 
 
-    //cells
+//creates calendar cells
 for (let a = 0; a < 7; a++) {
     if (i === 0 && a <firstDay) {
         
@@ -79,7 +90,4 @@ function daysInMonth(iMonth, iYear) {
 }
 }
 
-function dayClicked(el) {
-    el.style.backgroundColor = "blue";
-}
 
