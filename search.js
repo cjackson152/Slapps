123451940e20887e1a6b9ea45e4cec4581c53794
Ralph Magbanua <rmagbanua09@gmail.com>
2020-04-14 19:59:08 -0700

    for loop from recent - 10 max

--- search.js
+++ search.js
@@ -18,26 +18,30 @@ function artistResult(artist) {
         
         $("#artist-upcoming").empty();
         $("#artist-upcoming").append(artistName)
+        let arr = [];
+        // first start from array of most recently ended events
+        for (var i = response.length - 1; i > 0; i--) {
 
-        for (var i = 0; i < response.length; i++) {
-            if (i === 10) { break; }
+                if (i === response.length - 10) { break; }
+                arr.push(response[i]);
+                // dateTime = response[i].datetime;
+                // dateOnly = dateTime.slice(0,9)
+    
+                var buttons = $('<button>'+ "Save" + '</button>')
+                buttons.addClass("saveBtn") 
+                buttons.appendTo('#saveBtnCol'); 
+    
+                countryCol = $("<p>").text(response[i].venue.country)
+                venueCol = $("<p>").text(response[i].venue.name)
+                dateCol = $("<p>").text(response[i].datetime.slice(0,10))
+    
+                $("#artist-country").append(countryCol)
+                $("#artist-venue").append(venueCol)
+                $("#artist-date").append(dateCol)
+            }
+            // break out of outer loop when 10 events acquired
 
-            // dateTime = response[i].datetime;
-            // dateOnly = dateTime.slice(0,9)
-
-            var buttons = $('<button>'+ "Save" + '</button>')
-            buttons.addClass("saveBtn") 
-            buttons.appendTo('#saveBtnCol'); 
-
-            countryCol = $("<p>").text(response[i].venue.country)
-            venueCol = $("<p>").text(response[i].venue.name)
-            dateCol = $("<p>").text(response[i].datetime.slice(0,10))
-
-            $("#artist-country").append(countryCol)
-            $("#artist-venue").append(venueCol)
-            $("#artist-date").append(dateCol)
-        }
-
+console.log("arr: ", arr);
       });
     }
 
