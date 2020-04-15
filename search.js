e8ee356932d72498513de9c8b8f2f153e41feb4d
Ralph Magbanua <rmagbanua09@gmail.com>
2020-04-14 18:22:01 -0700

    search js, bandsintown api

---
+++ search.js
@@ -0,0 +1,41 @@
+
+function artistResult(artist) {
+    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp&date=past";
+    // var queryURL = "https://rest.bandsintown.com/artists/" + artist + "?app_id=codingbootcamp";
+
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    })
+      .then(function(response) {
+
+        // venueVal = response[0].venue.name;
+        // countryVal = response[0].venue.country;
+        // dateVal = response[0].dateTime;
+
+        artistName = $("<p>").text("Upcoming Events for " + artist)
+        
+        $("#artist-upcoming").empty();
+        $("#artist-upcoming").append(artistName)
+
+        for (var i = 0; i < response.length; i++) {
+            if (i === 10) { break; }
+            console.log(response)
+
+            countryCol = $("<p>").text(response[i].venue.country)
+            venueCol = $("<p>").text(response[i].venue.name)
+            dateCol = $("<p>").text(response[i].datetime)
+
+            $("#artist-event").append(countryCol)
+            $("#artist-venue").append(venueCol)
+            $("#artist-date").append(dateCol)
+        }
+
+      });
+    }
+
+$(".searchBtn").on("click", function(event) {
+    event.preventDefault();
+    inputArtist = $("#artist-input").val().trim();
+    artistResult(inputArtist);
+    });
